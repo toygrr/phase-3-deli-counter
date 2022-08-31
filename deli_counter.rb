bf8aa77fbb8c1613130878f17f166bab9eca5e5a
Tyler <toygrrr@gmail.com>
2022-08-31 15:47:50 -0700

    commit tree 9954c72fff897978f1439e3f34f1980030c83286

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+require 'pry'
+
+def line count
+    if count.length > 0
+        puts count.length
+    else 
+        puts "The line is currently empty."
+    end
+end
+
+def take_a_number(katz_deli, name)
+    katz_deli << name
+    katz_deli.each do |customer|
+        puts "The line is currently: #{customer}"
+    # binding.pry
+    end
+end
+
+take_a_number([], "tyler")
